d0f0874171a7f30d773103c2b5cc92df21d0f4a8
carlos <carloch1234@hotmail.com>
2025-02-19 15:12:52 -0500

    pk hasta clcick en find

--- Class1.cs
+++ Class1.cs
@@ -14,11 +14,18 @@ namespace TASk_loc1
         private readonly IWebDriver driver;
         private readonly WebDriverWait wait;
 
-        public Class1(IWebDriver driver) { this.driver = driver; wait = new WebDriverWait(driver, TimeSpan.FromSeconds(60));}
+        public Class1(IWebDriver driver) { 
+            this.driver = driver; wait = new WebDriverWait(driver, TimeSpan.FromSeconds(60));
+        }
+
+        public IWebElement Cookies => wait.Until(d => d.FindElement(By.Id("onetrust-accept-btn-handler")));
         public IWebElement Careers => wait.Until(d => d.FindElement(By.XPath("//*[@id=\"wrapper\"]/div[2]/div[1]/header/div/div/nav/ul/li[5]/span[1]/a")));
         public IWebElement Keywords => wait.Until(d => d.FindElement(By.Id("new_form_job_search-keyword")));
         public IWebElement Location => wait.Until(d => d.FindElement(By.XPath("//*[@id=\"jobSearchFilterForm\"]/div[2]/div/span[1]/span[1]/span")));
-        //public IWebElement All => wait.Until(d => d.FindElement(By.Id("select2-new_form_job_search-location-result-go7y-all_locations")));
+        public IWebElement All => wait.Until(d => d.FindElement(By.XPath("//*[@id=\"select2-new_form_job_search-location-results\"]/div[4]/div/div/li[1]/ul")));
+        public IWebElement Remote => wait.Until(d => d.FindElement(By.XPath("//*[@id=\"jobSearchFilterForm\"]/fieldset/div/p[1]")));
+
+        public IWebElement Find => wait.Until(d => d.FindElement(By.CssSelector("#jobSearchFilterForm > button")));
 
     }
 }
--- UnitTest1.cs
+++ UnitTest1.cs
@@ -19,10 +19,14 @@ namespace TASk_loc1
         {
             driver.Navigate().GoToUrl("https://www.epam.com/");
             driver.Manage().Window.Maximize();
+            epam.Cookies.Click();
             epam.Careers.Click();
             epam.Keywords.SendKeys("c#");
             epam.Location.Click();
-            //epam.All.Click();
+            epam.All.Click();
+            epam.Remote.Click();
+            epam.Find.Click();
+            Thread.Sleep(5000);
         }
         
         public void Dispose()
