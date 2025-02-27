3ea6c553058cdca1339aa5e47eac7905c7ff933f
carlos <carloch1234@hotmail.com>
2025-02-26 21:39:31 -0500

    Task 2 ok2 ig

--- obj/Debug/net8.0/Tasks.AssemblyInfo.cs
+++ obj/Debug/net8.0/Tasks.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tasks")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e6dd48989af922f2a226a8b31e6062c1ba84ba4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1998bde00b024e654c10decdefb1d42bc09bfb5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tasks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tasks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
