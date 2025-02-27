bd0740fc987e44a8d5362f33873fcca84913d5da
carlos <carloch1234@hotmail.com>
2025-02-26 20:06:16 -0500

    Completed1 added gitignore

--- obj/Debug/net8.0/Tasks.AssemblyInfo.cs
+++ obj/Debug/net8.0/Tasks.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tasks")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10e6f1116cc8a2abdc1db3bcc80fdd04af332fcf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c28c2f659927fb688848bc5020413b1a3ab3223")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tasks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tasks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
