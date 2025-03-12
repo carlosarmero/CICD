667764638757caf9f311a8dfc2f2a3288e84082e
carlos <carloch1234@hotmail.com>
2025-03-12 18:46:50 -0500

    git ignore

--- PageObjects/About.cs
+++ PageObjects/About.cs
@@ -23,7 +23,6 @@ namespace TASk_loc1.PageObjects
         public void ClickDown()
         {
             DownButton.Click();
-            Thread.Sleep(1200);
         }
         public void Dispose()
         {
