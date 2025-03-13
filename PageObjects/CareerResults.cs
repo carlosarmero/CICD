98f999382d0f885383a9964f9c7525936a0bd452
carlos <carloch1234@hotmail.com>
2025-03-12 19:02:29 -0500

    git ignore ok!

---
+++ PageObjects/CareerResults.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace TASk_loc1.PageObjects
+{
+    class CareerResults : IDisposable
+    {
+        private readonly IWebDriver driver;
+        private readonly WebDriverWait wait;
+
+        public CareerResults(IWebDriver driver, WebDriverWait wait)
+        {
+            this.driver = driver;
+            this.wait = wait;
+        }
+        public IList<IWebElement> Items => wait.Until(d => d.FindElements(By.ClassName("search-result__item-controls")));
+        public void ClickLast()
+        {
+            Items.Last().Click();
+        }
+        public void Dispose()
+        {
+            driver.Quit();
+        }
+    }
+}
---
+++ PageObjects/LastResultPage.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace TASk_loc1.PageObjects
+{
+    class LastResultPage : IDisposable
+    {
+        private readonly IWebDriver driver;
+        private readonly WebDriverWait wait;
+
+        public LastResultPage(IWebDriver driver, WebDriverWait wait)
+        {
+            this.driver = driver;
+            this.wait = wait;
+        }
+        public IWebElement body => wait.Until(d => d.FindElement(By.TagName("body")));
+        public string GetPageText()
+        {
+            return body.Text.ToLower();
+        }
+        public void Dispose()
+        {
+            driver.Quit();
+        }
+    }
+}
