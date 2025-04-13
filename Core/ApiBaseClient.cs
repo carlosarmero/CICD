5a2bcee339f49f1ca6901a3cdaf885833596f52f
carlos <carloch1234@hotmail.com>
2025-04-13 11:45:43 -0500

    check if the aseUrl is null or empty

--- Core/ApiBaseClient.cs
+++ Core/ApiBaseClient.cs
@@ -6,15 +6,19 @@ namespace TASk_loc1.Core
     public class ApiBaseClient : IDisposable
     {
         private readonly RestClient _client;
-       // public readonly JsonSerializerOptions _jsonOptions;
+        public readonly JsonSerializerOptions _jsonOptions;
 
         public ApiBaseClient(string baseUrl)
         {
+            if (string.IsNullOrWhiteSpace(baseUrl))
+            {
+                throw new ArgumentException("Base URL cannot be null or empty", nameof(baseUrl));
+            }
             _client = new RestClient(baseUrl);
-           /* _jsonOptions = new JsonSerializerOptions
+            _jsonOptions = new JsonSerializerOptions
             {
                 PropertyNameCaseInsensitive = true
-            };*/
+            };
         }
 
         public RestResponse ExecuteRequest(RestRequest request)
