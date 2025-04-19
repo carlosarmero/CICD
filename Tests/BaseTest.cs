ba40fc93945eb09b081785ca8e3fc3dc26548946
carlos <carloch1234@hotmail.com>
2025-04-19 10:12:35 -0500

    path5

--- Tests/BaseTest.cs
+++ Tests/BaseTest.cs
@@ -16,7 +16,7 @@ namespace TASk_loc1.Tests
             var potentialPaths = new[]
            {
                // Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "core", "files"),
-                Path.Combine(Directory.GetCurrentDirectory(), "core", "files"),
+                Directory.GetCurrentDirectory(),
                 "/home/runner/work/CICD/CICD/core/files" // GitHub Actions default
             };
 
